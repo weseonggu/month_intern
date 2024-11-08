4ccc9f55eb98b3e3c4a38f305d8efb4c36fe51c8
fkskdldh@naver.com <fkskdldh@naver.com>
2024-11-08 20:45:29 +0900

    feat JWT토큰 발금 검증 태스트

--- src/main/java/com/intellipick/intern/security/JWTConstants.java
+++
@@ -1,9 +0,0 @@
-package com.intellipick.intern.security;
-
-public class JWTConstants {
-	
-	public static final String JWT_KEY = "adfefdcdasdgbjklsbvlringirngringprojectfasdjkfbajbnkaf";
-	
-	public static final Long EXPIRATION_TIME = 30000000L;
-
-}
